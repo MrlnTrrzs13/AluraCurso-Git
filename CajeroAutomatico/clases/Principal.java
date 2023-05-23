2b5ed4fcc841b78fbac76b2cbcc48236ea9cc9ae
Marlon Torres <brand.trrzs13@gmail.com>
2023-05-22 23:37:03 -0600

    eliminando Practicas de Nodos y Labadora

--- CajeroAutomatico/clases/Principal.java
+++
@@ -1,9 +0,0 @@
-package clases;
-
-public class Principal {
-    public static void main(String[] args) {
-        Abstracta saldo = new Consulta();
-        saldo.setBalance(500);
-        saldo.Operations();
-    }
-}
